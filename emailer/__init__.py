f81be6f5038aa94bdc6a60fded2cc8c30a50d8ee
Andrey Bulgakov <mail@andreiko.ru>
2010-08-17 12:01:14 +0400

    first commit

---
+++ emailer/__init__.py
@@ -0,0 +1,177 @@
+__all__ = ['Account', 'Email', 'Attachment']
+
+import datetime, time
+from email.mime.multipart import MIMEBase, MIMEMultipart
+from email.header import Header
+from email.charset import Charset, QP
+from email.encoders import encode_quopri, encode_base64
+from mimetypes import guess_type
+from smtplib import SMTP, SMTP_SSL
+
+def is7bit(s):
+    for c in s:
+        if ord(c) > 127:
+            return False
+    return True
+
+class Account(object):
+    def __init__(self, email, fromname, server, login=None, password=None, port=25, ssl=False):
+        self.email = email
+        self.fromname = fromname
+        self.server = server
+        self.port = port
+        self.login = login
+        self.password = password
+        self.ssl = ssl
+
+    def send(self, emails):
+        if emails.__class__ == Email:
+            emails = [emails]
+        if len([e for e in emails if e.__class__ != Email]):
+            raise TypeError('emails must be Email or list of Email instances')
+
+        c = Charset('utf-8')
+        c.header_encoding = QP
+        c.body_encoding = 0
+        r = Charset('utf-8')
+        r.header_encoding = 0
+        r.body_encoding = 0
+
+        smtpclass = SMTP_SSL if self.ssl else SMTP
+        if self.server == 'localhost':
+            smtp = smtpclass(self.server)
+        else:
+            smtp = smtpclass(self.server, self.port)
+        if self.login and self.password:
+            smtp.login(self.login, self.password)
+        for email in emails:
+            email.normalize_email_list('rcpt')
+            email.normalize_email_list('cc')
+            mime1, mime2 = email.mimetype.split('/')
+            mainpart = MIMEBase(mime1, mime2)
+            mainpart.set_param('charset', 'utf-8')
+            if len(email.attachments):
+                message = MIMEMultipart(
+                            'related' if len(email.attachments) else 'mixed')
+                message.attach(mainpart)
+                del mainpart['mime-version']
+            else:
+                message = mainpart
+
+            message['Date'] = datetime.datetime.now().strftime(
+                '%a, %d %b %Y %H:%M:%S') + (" +%04d" % (time.timezone/-36,))
+
+            h = Header()
+            fromname = self.fromname.encode('utf-8')
+            h.append(fromname, r if is7bit(fromname) else c)
+            h.append('<%s>' % self.email, r)
+            message['From'] = h
+
+            message['To'] = email.get_emails_header('rcpt')
+            if len(email.cc):
+                message['CC'] = email.get_emails_header('cc')
+
+            subject = email.subject.encode('utf-8')
+            message['Subject'] = Header(subject, r if is7bit(subject) else c)
+
+            body = email.body.encode('utf-8')
+            mainpart.set_payload(body)
+
+            if is7bit(body):
+                mainpart['Content-Transfer-Encoding'] = '7bit'
+            else:
+                encode_quopri(mainpart)
+
+            for attachment in email.attachments:
+                if attachment.__class__ != Attachment:
+                    raise TypeError("invalid attachment")
+
+                mimetype = attachment.mimetype
+                if not mimetype:
+                    mimetype, encoding = guess_type(attachment.filename)
+                    if not mimetype:
+                        mimetype = 'application/octet-stream'
+                mime1, mime2 = mimetype.split('/')
+                part = MIMEBase(mime1, mime2)
+                part.set_param('name', attachment.filename)
+
+                del part['mime-version']
+
+                if attachment.id:
+                    part['Content-Disposition'] = 'inline'
+                else:
+                    part['Content-Disposition'] = 'attachment'
+                part.set_param('filename', attachment.filename,
+                                                'Content-Disposition')
+
+                if attachment.id:
+                    part['Content-ID'] = '<%s>' % attachment.id
+
+                part.set_payload(attachment.content)
+                encode_base64(part)
+
+                message.attach(part)
+
+            smtp.sendmail(self.email, [rcpt[1] for rcpt in email.rcpt] +
+                            [cc[1] for cc in email.cc], message.as_string())
+
+        smtp.quit()
+
+class Email(object):
+    def __init__(self, rcpt, subject, body, mimetype='text/plain', cc=[], attachments=[]):
+        self.rcpt = rcpt
+        self.cc = cc
+        self.subject = subject
+        self.body = body
+        self.mimetype = mimetype
+        self.attachments = attachments
+
+    def normalize_email_list(self, attr):
+        emails = self.__getattribute__(attr)
+        if type(emails) != list:
+            emails = [emails]
+        for i in range(len(emails)):
+            if type(emails[i]) == tuple:
+                if (type(emails[i][0]) not in (str, unicode)) or \
+                    (type(emails[i][1]) not in (str, unicode)):
+                        return TypeError('invalid "%s"' % attr)
+            else:
+                if type(emails[i]) not in (str, unicode):
+                    return TypeError('invalid "%s"' % attr)
+                emails[i] = (None, emails[i])
+        self.__setattr__(attr, emails)
+
+    def get_emails_header(self, attr):
+        c = Charset('utf-8')
+        c.header_encoding = QP
+        c.body_encoding = 0
+        r = Charset('utf-8')
+        r.header_encoding = 0
+        r.body_encoding = 0
+
+        h = Header()
+        self.normalize_email_list(attr)
+        emails = self.__getattribute__(attr)
+
+        for i in range(len(emails)):
+            name, email = emails[i]
+
+            if i:
+                h.append(',', r)
+
+            if name:
+                name = name.encode('utf-8')
+                h.append(name, r if is7bit(name) else c)
+                h.append('<%s>' % email, r)
+            else:
+                h.append(email, r)
+
+        return h
+
+class Attachment(object):
+    def __init__(self, filename, content, id=None, mimetype=None):
+        self.filename = filename
+        self.content = content
+        self.mimetype = mimetype
+        self.id = id
+
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+        name='emailer',
+        version='1.0',
+        description='Emailer class I use in my projects',
+        author='Andrey Bulgakov',
+        author_email='mail@andreiko.ru',
+        url='http://github.com/andreiko/emailer',
+        packages=['emailer'],
+    )
